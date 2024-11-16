904b0021657c426b02d9192d6111df6b40db9064
Jo Ao <jpmachacaz@gmail.com>
2024-11-16 23:16:52 +0100

    Add tests and refactor

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,10 +1,7 @@
-use log::warn;
-use std::env;
-
 use crate::Config;
+use log::warn;
 
-pub fn get_configuration() -> Config {
-    
+pub fn get_configuration(args: Vec<String>) -> Config {
     // Set defaults in case arguments are not provided
     let mut configuration = Config {
         test_mode: false,
@@ -13,10 +10,8 @@ pub fn get_configuration() -> Config {
         ws_port: "9002".to_string(),
     };
 
-    // Set config from start params
-    let args: Vec<String> = env::args().collect();
     if args.len() > 4 {
-        let ws_port = args[1].clone(); // Convert String to &str
+        let ws_port = args[1].clone();
         let serial_port = args[2].clone();
         let baudrate = args[3].clone();
         let test_arg = args[4].clone();
@@ -35,5 +30,55 @@ pub fn get_configuration() -> Config {
         };
     }
 
-    return configuration; 
+    return configuration;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_configuration_defaults() {
+        let args: Vec<String> = vec![];
+        let config = get_configuration(args);
+
+        assert_eq!(config.test_mode, false);
+        assert_eq!(config.serial_port, "/dev/ttyUSB0");
+        assert_eq!(config.baud_rate, 115200);
+        assert_eq!(config.ws_port, "9002");
+    }
+
+    #[test]
+    fn test_get_configuration_with_args() {
+        let args: Vec<String> = vec![
+            "program_name".to_string(),
+            "8080".to_string(),
+            "/dev/ttyS0".to_string(),
+            "9600".to_string(),
+            "true".to_string(),
+        ];
+        let config = get_configuration(args);
+
+        assert_eq!(config.test_mode, true);
+        assert_eq!(config.serial_port, "/dev/ttyS0");
+        assert_eq!(config.baud_rate, 9600);
+        assert_eq!(config.ws_port, "8080");
+    }
+
+    #[test]
+    fn test_get_configuration_invalid_baudrate() {
+        let args: Vec<String> = vec![
+            "program_name".to_string(),
+            "8080".to_string(),
+            "/dev/ttyS0".to_string(),
+            "invalid_baudrate".to_string(),
+            "false".to_string(),
+        ];
+        let config = get_configuration(args);
+
+        assert_eq!(config.test_mode, false);
+        assert_eq!(config.serial_port, "/dev/ttyS0");
+        assert_eq!(config.baud_rate, 115200); // Default baud rate
+        assert_eq!(config.ws_port, "8080");
+    }
 }
